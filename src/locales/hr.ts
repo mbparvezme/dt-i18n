69c7cfbdd29d839dd0f3cc7816d70d8c1e9dbd40
M B Parvez (Ron) <mbparvezme@gmail.com>
2025-07-26 16:20:57 +0600

    Readme file added!

---
+++ src/locales/hr.ts
@@ -0,0 +1,10 @@
+// hr.ts - Croatian
+import type { LocaleData } from '../types/index';
+
+const locale: LocaleData = {
+  numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  months: ['siječanj', 'veljača', 'ožujak', 'travanj', 'svibanj', 'lipanj', 'srpanj', 'kolovoz', 'rujan', 'listopad', 'studeni', 'prosinac'],
+  weekdays: ['nedjelja', 'ponedjeljak', 'utorak', 'srijeda', 'četvrtak', 'petak', 'subota'],
+};
+
+export default locale;
---
+++ src/locales/lv.ts
@@ -0,0 +1,10 @@
+// lv.ts - Latvian
+import type { LocaleData } from '../types/index';
+
+const locale: LocaleData = {
+  numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  months: ['janvāris', 'februāris', 'marts', 'aprīlis', 'maijs', 'jūnijs', 'jūlijs', 'augusts', 'septembris', 'oktobris', 'novembris', 'decembris'],
+  weekdays: ['svētdiena', 'pirmdiena', 'otrdiena', 'trešdiena', 'ceturtdiena', 'piektdiena', 'sestdiena'],
+};
+
+export default locale;
